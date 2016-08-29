971e6e138850355db0c5d842d67147ba17bcc4af
StPashik <stpashik@gmail.com>
2016-08-29 22:49:30 +0300

    pod init

---
+++ Pods/Target Support Files/Pods-vkyrse/Pods-vkyrse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_vkyrse : NSObject
+@end
+@implementation PodsDummy_Pods_vkyrse
+@end
---
+++ Pods/Target Support Files/Pods-vkyrse/Pods-vkyrse-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_vkyrseVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_vkyrseVersionString[];
+
